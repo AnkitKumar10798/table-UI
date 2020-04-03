205238350b52007ff3d6c9454adbdaa52c865946
AnkitKumar10798 <60109942+AnkitKumar10798@users.noreply.github.com>
2020-04-03 15:32:35 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {NewEntryComponent} from './new-entry/new-entry.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
